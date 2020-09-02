316bad4ed3bf4c02b9f0cfdbadf7af5e05cb49a8
Ella <29170065+fanteastick@users.noreply.github.com>
2020-09-02 14:21:32 -0700

    fixing 404 and maybe projects

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,12 +1,14 @@
 import TopHead from './TopHead.js'
 import ContactMe from './ContactMe.js'
 import React, { Component } from 'react';
+import Navbar from './Navbar.js'
 
 class Error404 extends Component {
     render() {
       return (
         <div className="Home">
           <TopHead />
+          <Navbar />
           <h1>Oops! Page not found!</h1>
           <ContactMe />
         </div> 
