4a461413772b4842da27decc53e488bdecad71a7
Eilleen <29170065+fanteastick@users.noreply.github.com>
2020-09-03 18:00:06 -0700

    fixing navbar part 2 define projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home.js'
+import Projects from './components/Projects.js'
 import Photos from './components/Photos.js'
 import Error from './components/Error404.js'
 
