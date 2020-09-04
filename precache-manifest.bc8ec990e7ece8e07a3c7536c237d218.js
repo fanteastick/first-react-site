c237f1173a09f6266faaedbbcb22ff60e073d790
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-04 06:51:21 +0000

    Deploy fanteastick/first-react-site to github.com/fanteastick/first-react-site.git:gh-pages

---
+++ precache-manifest.bc8ec990e7ece8e07a3c7536c237d218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d262f0d3797e5f65ded1709cca1c72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aec77922f1c2e6275167",
+    "url": "/static/css/main.9c05c1de.chunk.css"
+  },
+  {
+    "revision": "c98fba24cfc6ce574384",
+    "url": "/static/js/2.b8c8705e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b8c8705e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec77922f1c2e6275167",
+    "url": "/static/js/main.990bdc41.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  },
+  {
+    "revision": "780ef82c2c82fd5da1788ff09e5a0c75",
+    "url": "/static/media/eilleen-google-copy.780ef82c.jpg"
+  }
+]);
